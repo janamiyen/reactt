50d4ce0e2f29fea4f114ce09ced59134dd672a54
janamiyen <janamiyen@gmail.com>
2022-09-10 18:35:00 -0300

    -

--- src/items/ItemList.js
+++ src/items/ItemList.js
@@ -8,8 +8,8 @@ const ItemList = ({items}) => {
     <>
     
     <div className="d-flex flex-column border p-3" style={{width:"18rem"}} >
-    <a href='Home'>{items.imagen} </a>
     <h3>{items.name}</h3>
+   <div><img src={ process.env.PUBLIC_URL + '/img/' + items.imagen} alt={items.name} className="imgBurgerS"/></div> 
     <p>${items.precio}</p>
     <p>{items.descripcion}</p>
    <div><ItemCount stock='8' initial='1'/></div>
